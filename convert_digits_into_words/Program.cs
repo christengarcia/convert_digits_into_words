320bdcff751a5f0130b54382ac691051b3c5b815
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-28 17:02:54 +1100

    Initial commit

---
+++ convert_digits_into_words/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ *  C# Program to Convert Digits to Words
+ */
+
+using System;
+
+namespace convert_digits_into_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int nextdigit;
+            int numdigits;
+            int[] n = new int[20];
+
+            string[] digits = { "zero", "one", "two",
+                        "three", "four", "five",
+                        "six", "seven", "eight",
+                        "nine" };
+
+
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number: " + num);
+            Console.Write("Number in words: ");
+            nextdigit = 0;
+            numdigits = 0;
+            do
+            {
+                nextdigit = num % 10;
+                n[numdigits] = nextdigit;
+                numdigits++;
+                num = num / 10;
+            } while (num > 0);
+            numdigits--;
+            for (; numdigits >= 0; numdigits--)
+                Console.Write(digits[n[numdigits]] + " ");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
